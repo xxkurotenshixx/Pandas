2ab38d90d91dac767411d0cb5eb0dad3b4f90ec0
iwysiu <sakurablossom@blueblueworld.com>
2014-06-04 12:45:20 -0400

    comment out tree/bush stuff

--- src/scenes.js
+++ src/scenes.js
@@ -36,22 +36,22 @@ Crafty.scene('Game', function() {
 	this.occupied[this.player.at().x][this.player.at().y] = true;
  
 	// Place a pipe at every edge square on our grid of 16x16 tiles
-	for (var x = 0; x < Game.map_grid.width; x++) {
-		for (var y = 0; y < Game.map_grid.height; y++) {
-			var at_edge = x == 0 || x == Game.map_grid.width - 1 || y == 0 || y == Game.map_grid.height - 1;
+	//for (var x = 0; x < Game.map_grid.width; x++) {
+	//	for (var y = 0; y < Game.map_grid.height; y++) {
+	//		var at_edge = x == 0 || x == Game.map_grid.width - 1 || y == 0 || y == Game.map_grid.height - 1;
  
-			if (at_edge) {
-				// Place a tree entity at the current tile
-				Crafty.e('Tree').at(x, y)
-				this.occupied[x][y] = true;
-			} else if (Math.random() < 0.06 && !this.occupied[x][y]) {
-				// Place a bush entity at the current tile
-				var bush_or_rock = (Math.random() > 0.3) ? 'Bush' : 'Rock'
-				Crafty.e(bush_or_rock).at(x, y)
-				this.occupied[x][y] = true;
-			}
-		}
-	}
+	//		if (at_edge) {
+	//			// Place a tree entity at the current tile
+	//			Crafty.e('Tree').at(x, y)
+	//			this.occupied[x][y] = true;
+	//		} else if (Math.random() < 0.06 && !this.occupied[x][y]) {
+	//			// Place a bush entity at the current tile
+	//			var bush_or_rock = (Math.random() > 0.3) ? 'Bush' : 'Rock'
+	//			Crafty.e(bush_or_rock).at(x, y)
+	//			this.occupied[x][y] = true;
+	//		}
+	//	}
+	//}
 
 });
 
